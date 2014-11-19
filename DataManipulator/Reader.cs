f27f6284215dfbc4b8c77cf57d9ba7ec6dba48c1
Emil Jensen <estj92@gmail.com>
2014-11-19 13:20:03 +0100

    something somewhat works, but should be changed.

--- DataManipulator/Reader.cs
+++ DataManipulator/Reader.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.IO;
+using System.Diagnostics;
 
 namespace DataManipulator
 {
@@ -17,6 +18,11 @@ namespace DataManipulator
         public string DataFolder { get; private set; }
         public string TrainingFolder { get { return DataFolder + "/training"; } }
 
+        /// <summary>
+        /// Returns [movie, [user, rating]]
+        /// </summary>
+        /// <param name="file"></param>
+        /// <returns></returns>
         public Tuple<int, Dictionary<int, int>> ReadTrainingFile(string file)
         {
             var values = new Dictionary<int, int>();
@@ -43,6 +49,11 @@ namespace DataManipulator
             return new Tuple<int, Dictionary<int, int>>(index, values);
         }
 
+        /// <summary>
+        /// Returns [movie, [user, rating]]
+        /// </summary>
+        /// <param name="n"></param>
+        /// <returns></returns>
         public Dictionary<int, Dictionary<int, int>> ReadSeveralTrainingFiles(int n)
         {
             var filePaths = Directory.GetFiles(TrainingFolder);
@@ -77,6 +88,10 @@ namespace DataManipulator
             return movies;
         }
 
+        /// <summary>
+        /// Returns [movie, [users]]
+        /// </summary>
+        /// <returns></returns>
         public Dictionary<int, List<int>> ReadProbe()
         {
             var probes = new Dictionary<int, List<int>>();
@@ -101,6 +116,83 @@ namespace DataManipulator
             }
 
             return probes;
+        }
+
+        /// <summary>
+        /// Returns [movie, user]
+        /// </summary>
+        /// <param name="probes"></param>
+        /// <returns></returns>
+        public Dictionary<Tuple<int, int>, int> ProbesToPairs(Dictionary<int, List<int>> probes, IEnumerable<int> users)
+        {
+            int n = probes.Select(p => p.Value.Count).Sum();
+            var pairs = new Dictionary<Tuple<int, int>, int>(n);
+
+            foreach (var probe in probes)
+            {
+                foreach (var user in probe.Value)
+                {
+                    if (users.Contains(user))
+                    {
+                        pairs.Add(new Tuple<int, int>(probe.Key, user), -1);
+                    }
+                }
+            }
+
+            return pairs;
+        }
+
+        public Dictionary<Tuple<int, int>, int> ReadProbesAsPair(IEnumerable<int> movies, IEnumerable<int> users)
+        {
+            int i = 0;
+            var pairs = new Dictionary<Tuple<int, int>, int>();
+
+            using (StreamReader reader = new StreamReader(DataFolder + "/probe.txt"))
+            {
+                var colonTrim = new char[] { ':' };
+                var line = reader.ReadLine();
+                i++;
+                var ind = line.TrimEnd(colonTrim);
+                int movie = int.Parse(ind);
+                line = reader.ReadLine();
+                i++;
+
+                while (!reader.EndOfStream)
+                {
+                    Debug.WriteLine("i: " + i++);
+                    if (!movies.Contains(movie))
+                    {
+                        line = reader.ReadLine();
+                        while (!line.Contains(':') && !reader.EndOfStream)
+                        {
+                            line = reader.ReadLine();
+                            i++;
+                        }
+                        ind = line.TrimEnd(colonTrim);
+                        movie = int.Parse(ind);
+                        line = reader.ReadLine();
+                        i++;
+                    }
+                    else
+                    {
+                        while (!line.Contains(':') && !reader.EndOfStream)
+                        {
+                            int user = int.Parse(line);
+
+                            if (users.Contains(user))
+                            {
+                                pairs.Add(new Tuple<int, int>(movie, user), -1);
+                            }
+
+                            line = reader.ReadLine();
+                            i++;
+                        }
+                        movie = -1;
+                    }
+                }
+            }
+
+            return pairs;
         }
     }
 }
--- MainProgram/Program.cs
+++ MainProgram/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using DataManipulator;
+using System.Diagnostics;
 
 namespace MainProgram
 {
@@ -12,9 +13,48 @@ namespace MainProgram
         {
             Reader reader = new Reader(@"../../../netflix");
 
-            var files = reader.ReadSeveralTrainingFiles(10);
+            var tf = reader.ReadSeveralTrainingFiles(1100);
+
+            var trainingFiles = new Dictionary<int, Dictionary<int, int>>();
+
+            //trainingFiles = tf.Take(10);
+            //trainingFiles = trainingFiles.Union(tf.Skip(989).Take(10));
+
+            foreach (var item in tf.Take(10))
+            {
+                trainingFiles.Add(item.Key, item.Value);
+            }
+
+            foreach (var item in tf.Skip(995).Take(10))
+            {
+                trainingFiles.Add(item.Key, item.Value);
+            }
+
+
+            var trainingUsers = DistinctUsers(trainingFiles);
+            var trainingMovies = trainingFiles.Keys.ToList();
+
+
             var movies = reader.ReadMovies();
-            var probes = reader.ReadProbe();
+            //var probes = reader.ReadProbe();
+            //var pairs = reader.ProbesToPairs(probes, allUsers);
+            var pairs = reader.ReadProbesAsPair(trainingMovies, trainingUsers);
+        }
+
+        private static IEnumerable<int> DistinctUsers(Dictionary<int, Dictionary<int, int>> trainingFiles)
+        {
+            int i = 0;
+            var allUsers = Enumerable.Empty<int>();
+            foreach (var users in trainingFiles.Values)
+            {
+                allUsers = allUsers.Union(users.Keys);
+                //Debug.WriteLine("unioning " + i++);
+            }
+            allUsers = allUsers.OrderByDescending(u => u);
+
+
+            return allUsers;
+            //return new List<int>(allUsers);
         }
     }
 }
