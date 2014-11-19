b3d1d602f2da9c47bb6fb4535743b4fe8afdddc2
Emil Jensen <estj92@gmail.com>
2014-11-19 10:51:20 +0100

    Added trainingfilefolderpath property, readtrainingfile method.

--- DataManipulator/Reader.cs
+++ DataManipulator/Reader.cs
@@ -2,10 +2,41 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.IO;
 
 namespace DataManipulator
 {
     public class Reader
     {
+        public Reader(string trainingFilesFolderPath)
+        {
+            TrainingFilesFolderPath = trainingFilesFolderPath;
+        }
+
+        public string TrainingFilesFolderPath { get; private set; }
+
+        public Dictionary<int, int> ReadTrainingFile(string file)
+        {
+            var values = new Dictionary<int, int>();
+
+            using (StreamReader reader = new StreamReader(file))
+            {
+                reader.ReadLine();
+
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    var split = line.Split(new char[] { ',' });
+                    int user = int.Parse(split[0]);
+                    int rating = int.Parse(split[1]);
+
+                    values.Add(user, rating);
+                }
+            }
+
+            return values;
+        }
+
+
     }
 }
